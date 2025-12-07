487dbb44316dea6cb57edc3c4b2eb9073cf921e3
Nur Khevin <nurkhevinaja@gmail.com>
2025-12-07 23:19:56 +0700

    Change layout component from x-app-layout to x-layouts.admin-layout

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-layouts.admin-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Edit Akun Pengguna') }}
@@ -192,4 +192,4 @@
         }
     });
     </script>
-</x-app-layout>
+</x-layouts.admin-layout>
